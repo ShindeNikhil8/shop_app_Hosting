96aadc5a8898435b79408007828bebcb9420c4cb
ShindeNikhil8 <nikhil892003.93.20@gmail.com>
2025-09-09 15:05:52 +0530

    added frontend url

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://shop-app-hosting-jgkk.vercel.app",
   credentials: true
 }));
 app.use(express.json());
