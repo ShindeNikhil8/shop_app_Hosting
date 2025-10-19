3febfa97ef68c552dbb12962ebf5d9079b88f48b
ShindeNikhil8 <nikhil892003.93.20@gmail.com>
2025-10-20 00:08:29 +0530

    Updated product module

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,36 +1,35 @@
 import mongoose from "mongoose";
 
-const reviewSchema = new mongoose.Schema({
-  name: { type: String },
-  comment: { type: String },
-  rating: { type: Number, min: 0, max: 5 },
-});
-
-const productSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    desc: { type: String },
-    images: [
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  desc: { type: String },
+   images: [
       {
         url: { type: String, required: true },
         public_id: { type: String },
       },
-    ],
-    price: { type: Number, required: true },
-    oldPrice: { type: Number },
-    tag: { type: String },
-    mainCategory: { type: String, required: true },
-    subCategory: { type: String },
-    ageGroup: { type: String },
-    sku: { type: String },
-    stock: { type: Number, default: 0 },
-    likes: { type: Number, default: 0 },
-    rating: { type: Number, default: 0 },
-    reviews: [reviewSchema],
-  },
-  { timestamps: true }
-);
+    ], // array of image URLs
+  price: { type: Number, required: true },
+  oldPrice: { type: Number },
+  tag: { type: String }, // e.g., Featured, New Arrival
+  mainCategory: { type: String, required: true }, // Toys, Stationary, Gifts
+  subCategory: { type: String }, // e.g., Educational Toys
+  ageGroup: { type: String }, // e.g., 3-5, 6-8
+  sku: { type: String },
+  stock: { type: Number, default: 0 },
+  likes: { type: Number, default: 0 },
+  rating: { type: Number, default: 0 },
+  reviews: [
+  {
+    name: { type: String },
+    review: { type: String },
+    rating: { type: Number, min: 0, max: 5 },
+    comment: { type: String},
+  }
+],
+}, { timestamps: true });
 
 const Product = mongoose.model("Product", productSchema);
 
 export default Product;
+
