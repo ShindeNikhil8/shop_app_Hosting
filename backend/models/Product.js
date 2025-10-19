b36d2cac4ee443f4f08fae23bc353dd253880920
ShindeNikhil8 <nikhil892003.93.20@gmail.com>
2025-10-20 00:53:18 +0530

    Updated product module

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -3,13 +3,7 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   title: { type: String, required: true },
   desc: { type: String },
-  images: [
-  {
-    url: { type: String, required: true },
-    public_id: { type: String },
-  }
-],
-
+  images: [{ type: String }], // array of image URLs
   price: { type: Number, required: true },
   oldPrice: { type: Number },
   tag: { type: String }, // e.g., Featured, New Arrival
